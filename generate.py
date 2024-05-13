ebbe66051ff0db390930ee14b30c57f78c19a985
ZedanSaheer <zed.saheer5@gmail.com>
2024-05-13 16:32:57 +0300

    Trying system commands

--- generate.py
+++ generate.py
@@ -1,5 +1,13 @@
 import random
 
 coin = random.choice(["heads", "tails"])
-
 print(coin)
+
+cards = ["jack", "queen", "king"]
+random.shuffle(cards)
+
+for card in cards:
+    print(card)
+
+number = random.randint(1,50)
+print(number)
